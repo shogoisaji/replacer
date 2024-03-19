fba445b8d59d737546913aed344bc52bc43bab2f
shogoisaji <simacsimac9@gmail.com>
2024-03-19 17:20:51 +0900

    ✨  create temporary pages

---
+++ lib/pages/replace_edit_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReplaceEditPage extends StatelessWidget {
+  const ReplaceEditPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
