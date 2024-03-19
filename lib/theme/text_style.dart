35e8de1863e23c3cd6f13de561fbb6a599360f3c
shogoisaji <simacsimac9@gmail.com>
2024-03-20 08:53:50 +0900

    ⚡️ change text style overflow

--- lib/theme/text_style.dart
+++ lib/theme/text_style.dart
@@ -4,22 +4,32 @@ import 'package:replacer/theme/color_theme.dart';
 class MyTextStyles {
   static TextStyle get title {
     return const TextStyle(
-        fontSize: 54, fontWeight: FontWeight.w900, color: Color(MyColors.light), fontFamily: 'InterTight');
+        fontSize: 54,
+        fontWeight: FontWeight.w900,
+        color: Color(MyColors.light),
+        fontFamily: 'InterTight',
+        overflow: TextOverflow.ellipsis);
   }
 
   static TextStyle get subtitle {
     return const TextStyle(
-        fontSize: 32, fontWeight: FontWeight.w700, color: Color(MyColors.light), fontFamily: 'InterTight');
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: Color(MyColors.light),
+        fontFamily: 'InterTight',
+        overflow: TextOverflow.ellipsis);
   }
 
   static TextStyle get subtitleOrange {
     return const TextStyle(
-        fontSize: 32, fontWeight: FontWeight.w700, color: Color(MyColors.orange1), fontFamily: 'InterTight');
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: Color(MyColors.orange1),
+        fontFamily: 'InterTight',
+        overflow: TextOverflow.ellipsis);
   }
 
   static TextStyle get body {
-    return const TextStyle(
-      fontSize: 16,
-    );
+    return const TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis);
   }
 }
