742a667f883f8ea7cbc34c5ddc3c32a31e1a49a9
shogoisaji <simacsimac9@gmail.com>
2024-03-22 08:30:30 +0900

    ✨  add temporary export page

--- lib/pages/export_page.dart
+++ lib/pages/export_page.dart
@@ -1,10 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
-class ExportPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:replacer/states/capture_screen_state.dart';
+
+class ExportPage extends ConsumerWidget {
   const ExportPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final captureScreen = ref.watch(captureScreenStateProvider);
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Export'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                captureScreen != null
+                    ? CustomPaint(
+                        size: Size(size.width, size.height),
+                        painter: ImagePainter(captureScreen),
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// 画像を描画するためのCustomPainter ui.Imageを使用しているため
+class ImagePainter extends CustomPainter {
+  final ui.Image image;
+  ImagePainter(this.image);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    print('paint$size');
+    paintImage(canvas: canvas, rect: Rect.fromLTWH(0, 0, size.width, size.height), image: image);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
